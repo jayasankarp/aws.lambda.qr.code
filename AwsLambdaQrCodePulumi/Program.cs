58379ff455af2e28d986f17d2dcd0bf418558f80
Jay <jay@index.country>
2023-10-09 19:25:42 +0300

    Initial Commit

---
+++ AwsLambdaQrCodePulumi/Program.cs
@@ -0,0 +1,68 @@
+﻿using Pulumi;
+using System.Threading.Tasks;
+using Pulumi.Aws.Iam;
+using Pulumi.Aws.Lambda;
+
+class Program
+{
+   static Task<int> Main() => Deployment.RunAsync<LambdaStack>();
+}
+
+class LambdaStack : Stack
+{
+   public LambdaStack()
+   {
+      var lambda = new Function("awsLambdaQrCodeGenerator", new FunctionArgs
+      {
+         Runtime = "dotnet6",
+         Code = new FileArchive("../AwsQrCodeGenerator/bin/Release/net6.0/publish"),
+         Handler = "AwsLambdaQrCodeGenerator::AwsLambdaQrCodeGenerator.Function::FunctionHandler",
+         Role = CreateLambdaRole().Arn
+      });
+
+      this.Lambda = lambda.Arn;
+   }
+
+   [Output] public Output<string> Lambda { get; set; }
+
+   private static Role CreateLambdaRole()
+   {
+      var lambdaRole = new Role("lambdaRole", new RoleArgs
+      {
+         AssumeRolePolicy =
+            @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [
+                    {
+                        ""Action"": ""sts:AssumeRole"",
+                        ""Principal"": {
+                            ""Service"": ""lambda.amazonaws.com""
+                        },
+                        ""Effect"": ""Allow"",
+                        ""Sid"": """"
+                    }
+                ]
+            }"
+      });
+
+      var logPolicy = new RolePolicy("lambdaLogPolicy", new RolePolicyArgs
+      {
+         Role = lambdaRole.Id,
+         Policy =
+            @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [{
+                    ""Effect"": ""Allow"",
+                    ""Action"": [
+                        ""logs:CreateLogGroup"",
+                        ""logs:CreateLogStream"",
+                        ""logs:PutLogEvents""
+                    ],
+                    ""Resource"": ""arn:aws:logs:*:*:*""
+                }]
+            }"
+      });
+
+      return lambdaRole;
+   }
+}
